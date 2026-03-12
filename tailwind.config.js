9df90425692588df3fce9516fb558063c32bb4f1
TabarakoAkula <mishabor2367@gmail.com>
2026-03-12 20:37:05 +0200

    Fix css because of new architecture

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#657D9E',
+        secondary: '#9DB2CE',
+        background: '#CFDCED',
+        ind_good: '#5C8DFF',
+        ind_neutral: '#5A4FCF',
+        ind_bad: '#5846A8',
+      },
+    },
   },
   plugins: [],
-  presets: [require("nativewind/preset")],
 }
-
